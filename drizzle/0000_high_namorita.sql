72eef18f3807de1b97ae1a31015f62dcd831c5f0
OthmanAmoudi <red.fantastic@hotmail.com>
2023-06-05 11:30:27 +0300

    added prisma, schmea

--- drizzle/0000_high_namorita.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(256),
-	"email" varchar(256) UNIQUE NOT NULL,
-	"password" varchar(256),
-	"mobile_number" varchar(256),
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
-);
